f4efe9117cb15f00baab30effb2f913254bf7364
Yuki Higuchi <yuki@yhiguchi.dev>
2023-07-03 21:12:19 +0900

    chore: Add Reflection config

---
+++ quarkus/src/main/java/dev/yhiguchi/home_expense/config/MyReflectionConfig.java
@@ -0,0 +1,8 @@
+package dev.yhiguchi.home_expense.config;
+
+import dev.yhiguchi.home_expense.query.expense.ExpenseCriteria;
+import dev.yhiguchi.home_expense.query.expense.attribute.ExpenseAttributeCriteria;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(targets = {ExpenseCriteria.class, ExpenseAttributeCriteria.class})
+public class MyReflectionConfig {}
